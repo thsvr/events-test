71d81c2f36979341f69da3fea77552f103ad95af
thsvr <vr.ths.zd@gmail.com>
2019-12-17 19:15:18 -0300

    adding a form and basic style to sign-up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
 
   def show
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
