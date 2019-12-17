82a546d0bb0ae7b0260b563b8436c5a68296888d
thsvr <vr.ths.zd@gmail.com>
2019-12-17 19:35:50 -0300

    adding routes to login

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'users/create'
   get 'users/show'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
