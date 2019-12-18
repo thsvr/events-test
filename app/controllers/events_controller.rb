309ff4cd382a5e242f84fe452e8ff4940f68f8e0
thsvr <vr.ths.zd@gmail.com>
2019-12-18 18:08:33 -0300

    adding actions index, new, show and params to event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,23 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
   end
 
   def show
+    @event = Event.find(params[:id])
   end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:location, :event, :description, :date)
+  end
+
 end
