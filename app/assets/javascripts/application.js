4e4b373ac6c63d80dc987ecbb17b2d714e3d9fa0
thsvr <vr.ths.zd@gmail.com>
2019-12-18 16:33:04 -0300

    adding logout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require rails-ujs 
 //= require jquery
 //= require turbolinks
-//= require_tree .
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user #&& user.authenticate(params[:session][:name])
+    if user
       log_in user
       redirect_to user
     else
@@ -14,5 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to login_path
   end
 end
